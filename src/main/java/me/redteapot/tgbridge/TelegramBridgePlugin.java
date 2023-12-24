47489b13c0b5ca5c5075d67916e24b0a53670d96
Nikita Medvedev <some.redteapot@gmail.com>
2023-12-24 21:02:53 +0600

    Initial commit

---
+++ src/main/java/me/redteapot/tgbridge/TelegramBridgePlugin.java
@@ -0,0 +1,10 @@
+package me.redteapot.tgbridge;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class TelegramBridgePlugin extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("TelegramBridgePlugin init, lol!");
+    }
+}
