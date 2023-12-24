d0d731a4aab31770099d6f87ae907eec68fd03dc
Nikita Medvedev <some.redteapot@gmail.com>
2023-12-24 21:27:20 +0600

    Add Telegram plugin

--- src/main/java/me/redteapot/tgbridge/TelegramBridgePlugin.java
+++ src/main/java/me/redteapot/tgbridge/TelegramBridgePlugin.java
@@ -2,6 +2,7 @@ package me.redteapot.tgbridge;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+@SuppressWarnings("unused")
 public class TelegramBridgePlugin extends JavaPlugin {
     @Override
     public void onEnable() {
