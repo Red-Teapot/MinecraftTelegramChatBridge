95c13f8fdc409be62ec340b9e4cfaaf09fbe3bf5
Nikita Medvedev <some.redteapot@gmail.com>
2023-12-28 10:20:24 +0600

    Fix the test for Telegram sanitizer

--- src/test/java/me/redteapot/tgbridge/utils/SanitizingTests.java
+++ src/test/java/me/redteapot/tgbridge/utils/SanitizingTests.java
@@ -65,14 +65,14 @@ public class SanitizingTests {
                 \\|\\|spoiler\\|\\|
                 \\*bold \\_italic bold \\~italic bold strikethrough \\|\\|italic bold strikethrough spoiler\\|\\|\\~ \\_\\_underline italic bold\\_\\_\\_ bold\\*
                 \\[inline URL\\]\\(http://www\\.example\\.com/\\)
-                \\[inline mention of a user\\]\\(tg://user?id=123456789\\)
-                !\\[👍\\]\\(tg://emoji?id=5368324170671202286\\)
-                \\`inline fixed-width code\\`
+                \\[inline mention of a user\\]\\(tg://user?id\\=123456789\\)
+                \\!\\[👍\\]\\(tg://emoji?id\\=5368324170671202286\\)
+                \\`inline fixed\\-width code\\`
                 \\`\\`\\`
-                pre-formatted fixed-width code block
+                pre\\-formatted fixed\\-width code block
                 \\`\\`\\`
                 \\`\\`\\`python
-                pre-formatted fixed-width code block written in the Python programming language
+                pre\\-formatted fixed\\-width code block written in the Python programming language
                 \\`\\`\\`
                 """, sanitized);
     }
